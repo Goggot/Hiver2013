1f3f01a5cbd0dedbcf95881cd96cb980b4685152
Erwan <erwan.benard@gmail.com>
2013-03-06 10:09:22 -0500

    Mises à jour auto!

---
+++ PHP - JS/JS - La base/js/mon-javascript.js
@@ -0,0 +1,9 @@
+alert("boo");
+
+window.onload = changer;
+
+function changer(){
+    var element = document.getElementById("paragraphe");
+
+    element.style.color="red";
+}
---
+++ Test PyGame/test.py
@@ -0,0 +1,40 @@
+import pygame
+from pygame.locals import *
+
+continuer = True
+pygame.init()
+
+fenetre = pygame.display.set_mode((750, 770), RESIZABLE)
+
+fond = pygame.image.load("background.jpg").convert()
+fenetre.blit(fond, (0, 0))
+
+perso = pygame.image.load("perso.png").convert_alpha()
+pos = perso.get_rect()
+fenetre.blit(perso, pos)
+
+pygame.key.set_repeat(15, 15)
+pygame.display.flip()
+
+while continuer:
+    for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
+        if event.type == QUIT:     #Si un de ces événements est de type QUIT
+            continuer = False      #On arrête la boucle
+
+        elif event.type == KEYDOWN:     # si une touche du clavier est appuyée
+            if event.key == K_RIGHT:
+                pos = pos.move(3, 0)
+            elif event.key == K_LEFT:
+                pos = pos.move(-3, 0)
+            elif event.key == K_DOWN:
+                pos = pos.move(0, 3)
+            elif event.key == K_UP:
+                pos = pos.move(0, -3)
+
+
+#Re-collage
+    fenetre.blit(fond, (0,0))
+    fenetre.blit(perso, pos)
+
+#Rafraichissement
+    pygame.display.flip()
