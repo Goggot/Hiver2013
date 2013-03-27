31e8fcc5e63c54ad032a9ff9ca0f1e21f4037e47
Erwan <erwan.benard@gmail.com>
2013-03-27 14:07:01 -0400

    Mises à jour auto!

--- http/Lab - 02/element/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
-    <head>
-        <title>Vérification</title>
-        <link href="css/global.css" rel="stylesheet" type="text/css" media="screen" />
-	<script type="text/javascript" src="js/javascript.js"></script>
-    </head>
-    <body>
-	<div class="sectionAuthentification">
--- http/Lab - 02/prive.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once("action/priveAction.php");
-    execute();
-    
-    require_once("element/header.php");
-?>
