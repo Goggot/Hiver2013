5ae8c0464cc6a71dd8dd1282421b0b953cae175b
unknown <Erwan@407P34.DALLAS.B64>
2013-04-17 12:32:51 -0400

    sadgasg

---
+++ ReseauxMondiaux/Scripts/Multiple_Groups.ps1
@@ -0,0 +1,36 @@
+﻿$groupecsv = import-csv -path "c:\@Erwan\Hiver2013\ReseauxMondiaux\Scripts\L7DGroupe.csv" -delimiter ":"
+$utilcsv = import-csv -path "c:\@Erwan\Hiver2013\ReseauxMondiaux\Scripts\L7DUtilisateurs.csv" -delimiter ";"
+$domaine = "OU=Haiti,OU=Erwan,"+(get-addomain).distinguishedname
+
+foreach($item in $groupecsv)
+{
+    $nameGr = "H"+$item.Nom
+    Remove-ADGroup -identity $nameGr
+
+    write-host Création du groupe $nameGr
+    New-ADGroup -GroupScope $item.Etendue -Name $nameGr -GroupCategory $item.Type -path $domaine
+}
+
+foreach($item in $utilcsv)
+{
+    $nameUser = "H"+$item.Prenom
+    $upn = $item.Prenom+"@DALLAS.B64"
+    $passwd = convertto-securestring "AAAaaa111" -asplaintext -force
+    #Remove-ADUser -identity $nameUser
+
+    New-ADUser `
+    -Name $nameUser `
+    -SamAccountName $nameUser `
+    -UserPrincipalName $nameUser `    -GivenName $item.Nom `    -Path $domaine `
+    -AccountPassword $passwd `
+    -PasswordNeverExpires $true
+
+    $groupe = $item.Groupe.Split(",")
+    write-host $groupe
+
+    foreach ($item in $groupe)
+    {
+        $nameGr = "H"+$group
+        add-ADGroupMember -Identity $nameUser -Members $group
+    }
+}
---
+++ ReseauxMondiaux/Scripts/user+groupes.ps1
@@ -0,0 +1,21 @@
+﻿$c1 = "HaitiU1"
+$c2 = "HaitiU2"
+$g1 = "Haiti_grA"
+$g2 = "Haiti_grB"
+
+$passwd = convertto-securestring "AAAaaa111" -asplaintext -force
+
+remove-aduser -identity $c1
+remove-aduser -identity $c2
+Remove-ADGroup -identity $g1
+Remove-ADGroup -identity $g2
+
+New-ADUser `
+    -Name $c1 `
+    -SamAccountName $c1 `
+    -UserPrincipalName $c1@DALLAS.B64 `    -GivenName $c1 `    -Path "ou=Corpo,ou=Haiti,ou=Erwan,dc=DALLAS,dc=B64" `    -AccountPassword $passwd `
+    -PasswordNeverExpires $trueNew-ADUser `
+    -Name $c2 ` 
+    -SamAccountName $c2 `
+    -UserPrincipalName $c2@DALLAS.B64 `    -GivenName $c2 `    -Path "ou=Moron,ou=Corpo,ou=Haiti,ou=Erwan,dc=DALLAS,dc=B64" `    -AccountPassword $passwd `
+    -PasswordNeverExpires $trueNew-ADGroup -GroupScope DomainLocal -Name $g1 -GroupCategory Security -path "ou=Haiti,ou=Erwan,dc=DALLAS,dc=B64"New-ADGroup -GroupScope DomainLocal -Name $g2 -GroupCategory Security -path "ou=Haiti,ou=Erwan,dc=DALLAS,dc=B64"add-ADGroupMember -Identity $g1 -Members $c1,$c2Add-ADGroupMember -Identity $g2 -Members $c2
