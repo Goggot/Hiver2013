41e7db614ced3b790ac20cf26799f3d303be1dab
unknown <Erwan@407P34.DALLAS.B64>
2013-04-17 08:32:35 -0400

    assdfaf

---
+++ ReseauxMondiaux/Scripts/DNS+IIS.ps1
@@ -0,0 +1,14 @@
+﻿# Installer console DNS
+Install-WindowsFeature -name RSAT-DNS-Server -IncludeManagementTools -Restart
+
+# Ajouter adresse IP
+New-NetIPAddress -InterfaceAlias PciConfig -IPAddress 172.61.61.63 -AddressFamily IPv4 -PrefixLength 24
+
+# Créer site web par adresse
+New-Website -Name ADR1 -IPAddress 172.61.61.62 -PhysicalPath C:\_WEB\ADR1\ -Port 80
+
+# Créer site web par entete
+New-Website -Name ENT1 -HostHeader ENT1.Mexique.edu -IPAddress 172.61.61.61 -PhysicalPath C:\_WEB\ENT1\ -Port 80
+
+# Vérifier paramètre sites web
+Get-Website
---
+++ ReseauxMondiaux/Scripts/IP.ps1
@@ -0,0 +1,7 @@
+﻿$i = 1
+while ($i -eq 12){
+    $ip=10.57.61.+$i
+    New-NetIPAddress -InterfaceAlias OnBoardConfig -IPAddress $ip -AddressFamily IPv4 -PrefixLength 16
+    write-host Ajout de $ip
+    $i++
+}
