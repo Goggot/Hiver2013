e0075fd98287ae82d3daf80af28e3bb3dd6714e8
unknown <H12PALANER@.decinfo.cvm>
2013-04-25 15:18:21 -0400

    as

---
+++ ADF/LabFinalADF/ViewController/src/view/Login.java
@@ -0,0 +1,17 @@
+package view;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+public class Login
+{
+  public Login()
+  {
+  }
+
+  public void it1_validator(FacesContext facesContext,
+                            UIComponent uIComponent, Object object)
+  {
+    // Add event code here...
+  }
+}
