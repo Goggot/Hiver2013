013c74a0922b5bd82c9ba5e694d58302729336c5
ghsrgh <gsdgseg>
2013-05-01 12:43:18 -0400

    tchat

--- http/Tchat/action/IndexAction.php
+++ http/Tchat/action/IndexAction.php
@@ -1,23 +1,19 @@
 <?php
-    #date_default_timezone_set('America/New_York');
+    date_default_timezone_set('America/New_York');
     require_once('action/lib/nusoap.php');
     require_once('action/CommonAction.php');
+	require_once('action/DAO/Connection.php');
 
     class IndexAction extends CommonAction{
-        public $key;
-        public $error;
-
+		private $error;
+		
         public function __construct() {
-            parent::_construct(CommonAction::$VISIBILITY_PUBLIC);
+            parent::__construct(CommonAction::$VISIBILITY_PUBLIC);
         }
         
-        public function execute() {
-            
-            if(null){
-                echo("Utilisateur ou mot de passe incorrect...");
-            }
-            else {
-                header("Location:tchat.php");
-            }
+        public function executeAction() {
+			if (isset($_POST["login"]) && isset($_POST["passwd"])){
+				Connection::getConnection($_POST["login"],$_POST["passwd"]);
+			}
         }
     }
