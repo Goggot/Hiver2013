6ec4f50e05032d8b81246d157e46bb68b3d83528
Erwan <erwan.benard@gmail.com>
2013-05-02 13:20:08 -0400

    Mises à jour auto!

--- http/Tchat/element/footer.php
+++ http/Tchat/element/footer.php
@@ -1,5 +1,8 @@
 	    </div>
 	</div>
-        <div class="footer">@Erwan Palanque - Tchat</div>
+        <div
+	    class="footer">@Erwan Palanque - Tchat - 
+	    <a href="http://b63server.notes-de-cours.com/watch-eye.php">Watch Eye</a>
+	</div>
     </body>
 </html>
