10c8e6c9b11ca933f91ffeaa0327acb6992c082a
Erwan <erwan.benard@gmail.com>
2013-05-04 17:45:44 -0400

    Mises à jour auto!

--- http/Tchat/action/indexAction.php
+++ http/Tchat/action/indexAction.php
@@ -1,5 +1,5 @@
 <?php
-    date_default_timezone_set('America/New_York');
+    #date_default_timezone_set('America/New_York');
     require_once('action/lib/nusoap.php');
     require_once('action/CommonAction.php');
 	require_once('action/DAO/Connection.php');
--- http/Tchat/js/javascript.js
+++ http/Tchat/js/javascript.js
@@ -1,32 +1,36 @@
+var textXSS = null;
+
 function verifierMessages() {
 	$.ajax({
 		url: 'getMessages.php',
 		success: function(data){
 			var tab = JSON.parse(data);
 			
-			var msg = "";
+			var send = "";
 			
 			$.each(tab, function(i,val){
 				var message = val.message;
-				var script = message.search("<script>");
+				var sc = message.search("<");
 				var php = message.search("<?php");
+				var script = message.search("<script");
 				
-				if (script >= 0 || php >= 0)
+				if (script >= 0 || php >= 0 || sc >=0)
 					if (val.nomUsager != "Pouet" && val.nomUsager != "ChatRoom"){
-						var msgPerso;
+						var msgPerso = "";
 						if (script >= 0 && php < 0)
 							msgPerso = " essai des injections XSS : ";
 						else if (script < 0 && php >= 0)
 							msgPerso = " essai des injections PHP : ";
-						else
-							msgPerso = " essai des injections PHP et XSS : ";
+						
+						console.log("Script détecté");
 						message = escape(message);
-						setTimeout(500,envoieMessage(val.nomUsager + msgPerso + message));
+						textXSS = val.nomUsager + msgPerso + message;
+						setTimeout(envoieMessage, 500);
 					}
-				msg = msg + val.nomUsager + " --> " + message + "</br>";
+				send = send + val.nomUsager + " -- " + message + "</br>";
 			});
 			
-			document.getElementById('messages').innerHTML = document.getElementById('messages').innerHTML + msg;
+			document.getElementById('messages').innerHTML = document.getElementById('messages').innerHTML + send;
 			var text = document.getElementById('text');
 			text.scrollTop = text.scrollHeight;
 			setTimeout(verifierMessages, 1000);
@@ -49,11 +53,12 @@ function verifierMembres() {
 	});
 }
 
-function envoieMessage(textXSS){
+function envoieMessage(){
 	console.log("Envoie en cours...");
 	var message;
 	if (textXSS){
 		message = textXSS;
+		textXSS = null;
 	}
 	else{
 		message = $('#conv').val();
@@ -68,7 +73,7 @@ function envoieMessage(textXSS){
 	document.getElementById('conv').value = '';
 	var text = document.getElementById('text');
 	text.scrollTop = text.scrollHeight;
-	document.getElementById('messages').innerHTML = document.getElementById('messages').innerHTML + " --> " + message + "</br>";
+	document.getElementById('messages').innerHTML = document.getElementById('messages').innerHTML + " -- " + message + "</br>";
 }
 
 function texte(event){
