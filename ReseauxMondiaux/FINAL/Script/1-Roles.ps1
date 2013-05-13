16cfc0698ea29fbef72e5f9eafdfc4f56678b1b4
unknown <Erwan@407PF4.TheThrone.PRO>
2013-05-13 11:23:58 -0400

    reseau

--- ReseauxMondiaux/FINAL/Script/1-Roles.ps1
+++ ReseauxMondiaux/FINAL/Script/1-Roles.ps1
@@ -2,11 +2,11 @@
 
 Rename-Computer -NewName "407PF4"
 
+Set-ExecutionPolicy unrestricted
 Install-WindowsFeature -Name "ad-domain-services" -IncludeAllSubFeature -IncludeManagementTools
 Install-WindowsFeature -Name "dns" -IncludeAllSubFeature -IncludeManagementTools
 Install-WindowsFeature -Name "web-server" -IncludeAllSubFeature -IncludeManagementTools
 Install-WindowsFeature -Name "web-ftp-server" -IncludeAllSubFeature -IncludeManagementTools
-Install-WindowsFeature -Name "smtp-server" -IncludeAllSubFeature -IncludeManagementTools
 Install-WindowsFeature -Name "RSAT-ADDS-Tools" -IncludeAllSubFeature -IncludeManagementTools
 Install-WindowsFeature -Name "Remote-Desktop-Services" -IncludeAllSubFeature -IncludeManagementTools
 Install-WindowsFeature -Name "RSAT-AD-PowerShell" -IncludeAllSubFeature -IncludeManagementTools
--- ReseauxMondiaux/FINAL/Script/3-Admin+DM.ps1
+++ ReseauxMondiaux/FINAL/Script/3-Admin+DM.ps1
@@ -1,4 +1,4 @@
-##### Jonction DM et création Admin perso
+##### Jonction DM et création Admin perso #####
 
 ##### Rejoindre le Controleur de domaine #####
 Add-Computer -DomainName THETHRONE.PRO -ComputerName 407PF4
@@ -10,4 +10,5 @@ $login = "Erwan"
 New-ADUser `
     -Name $login `
     -SamAccountName $login `
-    -UserPrincipalName $login@THETHRONE.PRO `    -GivenName $login `    -DisplayName $login `    -AccountPassword $passwd `    -Path "ou=Users,dc=THETHRONE,dc=PRO" `    -PasswordNeverExpires $true `    -Enabled $trueAdd-ADGroupMember -Identity "Administrateurs" -Members $loginAdd-ADGroupMember -Identity "Administrateurs de l’entreprise" -Members $loginAdd-ADGroupMember -Identity "Administrateurs du schéma" -Members $loginAdd-ADGroupMember -Identity "Admins du domaine" -Members $loginAdd-ADGroupMember -Identity "Utilisateurs du domaine" -Members $login
+    -UserPrincipalName $login@THETHRONE.PRO `    -GivenName $login `    -DisplayName $login `    -AccountPassword $passwd `    -Path "cn=Users,dc=THETHRONE,dc=PRO" `    -PasswordNeverExpires $true `    -Enabled $trueAdd-ADGroupMember -Identity "Administrateurs" -Members $loginAdd-ADGroupMember -Identity "Administrateurs de l’entreprise" -Members $loginAdd-ADGroupMember -Identity "Administrateurs du schéma" -Members $loginAdd-ADGroupMember -Identity "Admins du domaine" -Members $login
+Add-ADGroupMember -Identity "Propriétaires créateurs de la stratégie de groupe" -Members $login
