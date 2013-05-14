8a6b609024b21302b87bade97163f650fe239fc4
Erwan <erwan.benard@gmail.com>
2013-05-14 11:02:30 -0400

    Mises à jour auto!

--- http/Facturation/action/RecoverAction.php
+++ http/Facturation/action/RecoverAction.php
@@ -9,10 +9,10 @@
  *--------------------------------------------------- */
 
 
-    require_once("action/CommonAction.php");
+	require_once("action/CommonAction.php");
 	require_once("action/DAO/ClientDAO.php");
 
-    class RecoverAction extends CommonAction{
+	class RecoverAction extends CommonAction {
 		public function __construct(){
 			getSession();
 			parent::__construct(CommonAction::$VISIBILITY_MEMBER);
@@ -22,23 +22,24 @@
 			if (isset($_SESSION["usernameRecover"]) && isset($_POST["newPasswd"]) && isset($_POST["newPasswd2"])){
 				if ($_POST["newPasswd"] === $_POST["newPasswd2"]){
 					$passwd = $_POST["newPasswd"];
-					ClientDAO::recoverPasswd($passwd, $_SESSION["usernameRecover"])
+					ClientDAO::recoverPasswd($passwd, $_SESSION["usernameRecover"]);
 					header('location:index.php&logout=true');
 					echo "Changement réussi !";
 					exit;
 				}
-				else
+				else{
 					echo "Les mots de passes ne concordent pas...";
+				}
 			}
 		}
 
 		private function getSession(){
 			if (isset($_GET["forgot_key"])){
-				$user = ClientDAO::findVisibility($_GET["forgot_key"])
+				$user = ClientDAO::findVisibility($_GET["forgot_key"]);
 				if ($user[0] != FALSE){
 					$_SESSION["loggedIn"] = $user[0];
 					$_SESSION["usernameRecover"] = $user[1];
 				}
 			}
 		}
-    }
+	}
