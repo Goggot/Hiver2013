e226ccfa5d43672f4858f554ce85505e46289899
unknown <Erwan@407PF4.TheThrone.PRO>
2013-05-15 10:52:33 -0400

    reseau

--- ReseauxMondiaux/FINAL/Script/2-Reseau.ps1
+++ ReseauxMondiaux/FINAL/Script/2-Reseau.ps1
@@ -1,12 +1,28 @@
-﻿##### 2 - Réseau et DM serveur perso #####
+﻿###########################################
+#####         Erwan Palanque          #####
+#####          15 mai 2013            #####
+##### Configuration des cartes réseau #####
+#####        Serveur : 407PF4         #####
+###########################################
+
+$ipPrincipal = "172.24.48.173"
+$ipSup = "172.24.48.175"
+$ipCD = "172.24.48.2"
+$ipRoot = "172.24.48.1"
+$pref = 20
+$p = "IPv4"
+$c1 = "SEGA"
+$c2 = "OnBoard"
+
 
 ##### Renommer les cartes réseau #####
-Rename-NetAdapter -Name Ethernet -NewName OnBoard
-Rename-NetAdapter -Name "Ethernet 2" -NewName SEGA
+Rename-NetAdapter -Name Ethernet -NewName $c2
+Rename-NetAdapter -Name "Ethernet 2" -NewName $c
 
 ##### Attribuer les adresses #####
-New-NetIPAddress -InterfaceAlias SEGA -IPAddress 172.24.48.173 -AddressFamily IPv4 -DefaultGateway 172.24.48.1 -PrefixLength 24
-Set-DnsClientServerAddress -InterfaceAlias SEGA -ServerAddresses 172.24.48.2
-Disable-NetAdapter -Name OnBoard
+New-NetIPAddress -InterfaceAlias $c1 -IPAddress $ipPrincipal -AddressFamily $p -DefaultGateway $ipRoot -PrefixLength $pref
+Set-DnsClientServerAddress -InterfaceAlias $c1 -ServerAddresses $ipCD
+Disable-NetAdapter -Name $c2
 
 ##### Ajouter les adresses supplémentaires #####
+New-NetIPAddress -InterfaceAlias $c1 -IPAddress $ipSup -AddressFamily $p -PrefixLength $pref
